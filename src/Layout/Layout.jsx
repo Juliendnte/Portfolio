5b7123913d5a7bbf0ecbcdc7433a3b926ac299ec
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-10-05 23:47:08 +0200

    hebergement

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,14 +1,12 @@
 import { Container } from './LayoutStyle'
 import Header from '../Components/Header/Header'
 import Main from '../Components/Main/Main'
-import Footer from '../Components/Footer/Footer'
 
 export const Layout = () => {
     return (
         <Container>
             <Header/>
             <Main/>
-            <Footer/>
         </Container>
     )
 }
