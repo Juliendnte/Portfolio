c06df43190b33f39a3e8d6dca0a8e7fc9c9bc5a0
Juliendnte <145002789+Juliendnte@users.noreply.github.com>
2024-12-29 22:18:50 +0100

    Updates

--- assets/index-DmB3EZqH.js
+++ assets/index-DmB3EZqH.js
@@ -478,7 +478,7 @@ Error generating stack: `+h.message+`
     text-decoration: none;
     color: #fff;
     font-weight: bold;
-`,Contact=()=>jsxRuntimeExports.jsx(ContainerContact,{className:"inline-grid items-center overflow-hidden hover:transition-all font-medium max-[980px]:px-[40px] px-[60px] min-[1112px]:h-[164px] min-[980px]:h-[140px] max-[980px]:h-[120px] max-[780px]:px-[28px] max-[780px]:h-[79px] rounded-[100px] border-2 border-transparent btnbo max-[406px]:text-[30px] min-[1112px]:text-[100px] min-[980px]:text-[90px] max-[980px]:text-[70px] max-[780px]:text-[36px] tracking-[-2px] ",id:"Contact",children:jsxRuntimeExports.jsx(ContactA,{href:"mailto:julien.dante@ynov.com",children:"Contact Me"})}),FooterContainer=dt.footer`
+`,Contact=()=>jsxRuntimeExports.jsx(ContainerContact,{className:"inline-grid items-center overflow-hidden hover:transition-all font-medium max-[980px]:px-[40px] px-[60px] min-[1112px]:h-[164px] min-[980px]:h-[140px] max-[980px]:h-[120px] max-[780px]:px-[28px] max-[780px]:h-[79px] rounded-[100px] border-2 border-transparent btnbo max-[406px]:text-[30px] min-[1112px]:text-[100px] min-[980px]:text-[90px] max-[980px]:text-[70px] max-[780px]:text-[48px] tracking-[-2px] ",id:"Contact",children:jsxRuntimeExports.jsx(ContactA,{href:"mailto:julien.dante@ynov.com",children:"Contact Me"})}),FooterContainer=dt.footer`
     border-top: 4px dotted ${e=>e.theme.colors.background2};
     width: 100%;
 `,FooterParagraph=dt.p`
